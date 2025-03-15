e724206312a9ad273975e8d341eee94faa3498cf
Johan Giraldo Duque <johan.dgiraldo@gmail.com>
2025-03-15 17:22:02 -0500

    primera clase

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// // variables 
+// let nombre = "Johan "
+// let edad = 25
+// let dia = true
+
+// nombre = "Juan"
+
+// console.log(nombre)
+// console.log(edad)
+
+// constante --> no puedo luego modificar el archivo 
+// const color = "rojo"
+// const deporte = "futbol"
+// const jugadores = 21
+
+// console.log("el color del equipo es de " + color + " y el deporte es " + deporte + "con " + jugadores + " jugadores")
+
+// const comision = prompt ("Ingrese comision")
+// let curso = prompt("Ingrese el curso ")
+
+// console.log("Bienvenidos a la comision "+ comision+ " del curso de " + curso)
+// alert("Bienvenidos a la comision "+ comision+ " del curso de " + curso)
+
+let numeroA = parseInt(prompt("Ingres el primer numero"))
+let numeroB = parseInt(prompt("Ingresa el segundo numero"))
+
+let resultado = numeroA + numeroB
+console.log(resultado)
